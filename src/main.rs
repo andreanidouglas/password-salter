55e1ecd731e9c16f7655bbf965125f795c132ed4
Douglas A <andreani.dr@gmail.com>
2023-03-08 19:13:03 -0300

    feat(cli): new cli paraments and ability to check password against hash

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,72 @@
-use std::env;
 use argon2::{
-        password_hash::{
-                rand_core::OsRng,
-                SaltString, PasswordHash, PasswordHasher, PasswordVerifier
-    }, 
-    Argon2
+    password_hash::{rand_core::OsRng, PasswordHasher, SaltString},
+    Argon2, PasswordHash, PasswordVerifier,
 };
+use clap::{Parser, Subcommand};
 
-fn main() -> anyhow::Result<()> {
-    
-    let args: Vec<String> = env::args().collect(); 
-    if args.len() < 2 {
-        eprintln!("usage ./{} <password>", args[0]);
-        anyhow::bail!("missing command line arguments");
-    }
+#[derive(Subcommand, Debug)]
+enum VerifyArgs {
+    Verify {
+        #[arg(short='r', long)]
+        hash_password: String,
+    },
+}
 
-    let password = args[1].as_bytes();
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    password: String,
 
+    #[command(subcommand)]
+    verify: Option<VerifyArgs>,
+}
+
+fn create_hash(password: &[u8]) -> anyhow::Result<String> {
     let salt = SaltString::generate(&mut OsRng);
 
     let argon2 = Argon2::default();
     let password_hash = match argon2.hash_password(password, &salt) {
         Ok(v) => v.to_string(),
-        Err(e) => anyhow::bail!("{e}")
+        Err(e) => anyhow::bail!("Could not create hash: {e}"),
     };
+    Ok(password_hash.to_string())
+}
 
+fn verify(password_hash: String, password: &[u8]) -> anyhow::Result<bool> {
     let parsed_hash = match PasswordHash::new(&password_hash) {
         Ok(v) => v,
-        Err(e) => anyhow::bail!("{e}")
+        Err(e) => anyhow::bail!("could not verify hash: {e}"),
     };
 
+    if Argon2::default()
+        .verify_password(password, &parsed_hash)
+        .is_ok()
+    {
+        Ok(true)
+    } else {
+        Ok(false)
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let cli = Args::parse();
+
+    match &cli.verify {
+        Some(VerifyArgs::Verify { hash_password }) => {
+            if verify(String::from(hash_password), cli.password.as_bytes())?{
+                println!("OK: Password and hash do match.");
+            } else {
+                println!("WARN: Password and hash do not match.")
+            }
+            
+            Ok(())
+        },
+        None => {
+            let hash = create_hash(cli.password.as_bytes())?;
+            println!("OK: {hash}");
+            Ok(())
+        }
+    }
 
 
-    assert!(Argon2::default().verify_password(password, &parsed_hash).is_ok());
-    println!("OK: {}\nSalt: {}", password_hash, salt);
-
-    Ok(())
 }
